3eb7586138da33564ccf9de112d7a6156c96cea1
Evan Moore <etmoore@gmail.com>
2016-10-04 22:23:20 -0600

    remove whitespace in gruntfile

--- responsive-images/Project-Part-1-Start/Gruntfile.js
+++ responsive-images/Project-Part-1-Start/Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
           sizes: [{
             /*
             Change these:
-            
+
             width: ,
             suffix: ,
             quality:
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
       },
     },
   });
-  
+
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
